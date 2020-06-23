21e3d3885402e0f8343efdaf24dbdf3203661b5a
VerSign010 <54786470+VerSign010@users.noreply.github.com>
2020-06-23 07:57:56 +0000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,7 +4,7 @@ cat <<-EOF > /trojan/config.json
 {
     "run_type": "server",
     "local_addr": "0.0.0.0",
-    "local_port": ${PORT},
+    "local_port": 443,
     "remote_addr": "0.0.0.0",
     "remote_port": 80,
     "password": [
